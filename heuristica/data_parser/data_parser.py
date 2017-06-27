31e63e6cca722357c3818839828ff5fc4e1d1536
Lucas Leal <lleal48 [at] gmail [dot] com>
2017-06-27 08:18:29 -0300

    removes regex from parser to improve performance

--- heuristica/data_parser/data_parser.py
+++ heuristica/data_parser/data_parser.py
@@ -1,6 +1,3 @@
-import sys, re
-
-
 def get_parameters(filename):
     input_file = open(filename, "r")
     lines = input_file.read().splitlines(1)
@@ -12,48 +9,34 @@ def get_parameters(filename):
     profits = {}
     conflicts = {}
 
-    exps = "(param|set)\s*(c|n|: V : p w|E)\s*:=\s*(\d*);*"
     for line in lines:
-        m1 = re.match(exps, line)
-        try:
-            if m1.group(1) == "param":
-                if m1.group(2) == "n":
-                    n = int(m1.group(3))
-                elif m1.group(2) == "c":
-                    c = int(m1.group(3))
-                elif m1.group(2) == ": V : p w":
-                    for l in range(lines.index(line), lines.index(line)+n+1):
-                        m2 = re.match("\s*(\d+)\s*(\d+)\s*(\d+)", lines[l])
-                        try:
-                            it = int(m2.group(1))
-                            p = int(m2.group(2))
-                            w = int(m2.group(3))
-                            items.add(it)
-                            weights[it] = w
-                            profits[it] = p
-                        except:
-                            continue
-            elif m1.group(1) == "set":
-                for l in range(lines.index(line), len(lines)+1):
-                    m2 = re.match("\s*(\d+)\s*(\d+)", lines[l])
-                    try:
-                        k1 = int(m2.group(1))
-                        k2 = int(m2.group(2))
-                        if k1 not in conflicts:
-                            conflicts[k1] = set()
-                            conflicts[k1].add(k2)
-                        else:
-                            conflicts[k1].add(k2)
-                        if k2 not in conflicts:
-                            conflicts[k2] = set()
-                            conflicts[k2].add(k1)
-                        else:
-                            conflicts[k2].add(k1)
-                    except:
-                        continue
-            else:
-                pass
-        except:
-            continue
-    
+        if "param n :=" in line:
+            n = int(line.split()[-1].split(";")[0])
+        elif "param c :=" in line:
+            c = int(line.split()[-1].split(";")[0])
+        elif "param : V : p w :=" in line:
+            for i in range(lines.index(line)+1, lines.index(line)+n+1):
+                values = lines[i].split()
+                if len(values) == 3:
+                    it = int(values[0])
+                    items.add(it)
+                    profits[it] = int(values[1])
+                    weights[it] = int(values[2])
+        elif "set E :=" in line:
+            for i in range(lines.index(line)+1, len(lines)):
+                values = lines[i].split()
+                if len(values) == 2:
+                    k1 = int(values[0])
+                    k2 = int(values[1])
+                    if k1 not in conflicts:
+                        conflicts[k1] = set()
+                        conflicts[k1].add(k2)
+                    else:
+                        conflicts[k1].add(k2)
+                    if k2 not in conflicts:
+                        conflicts[k2] = set()
+                        conflicts[k2].add(k1)
+                    else:
+                        conflicts[k2].add(k1)
+
     return n, c, items, weights, profits, conflicts
